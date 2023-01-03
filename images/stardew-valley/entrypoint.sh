dd736d99be6319f231eca6baa6866723ec1c0941
Small_Ku <small_ku@outlook.com>
2023-01-04 03:17:36 +0800

    fix: setup in install script

--- images/stardew-valley/entrypoint.sh
+++ images/stardew-valley/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /home/container
 
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
